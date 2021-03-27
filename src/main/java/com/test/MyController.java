040a7bc0703faed47676f14fd8eadb76ad4becd6
pantingye <15057488152@163.com>
2021-03-27 16:22:36 +0800

    myTest项目第一次提交

---
+++ src/main/java/com/test/MyController.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pan'ting'ye
+ * @since 2021/3/25 23:22
+ */
+@RestController
+@RequestMapping("/test")
+public class MyController {
+    @RequestMapping("/get")
+    @ResponseBody
+    public String getName() {
+        return "1111111!!!!!!";
+    }
+}
